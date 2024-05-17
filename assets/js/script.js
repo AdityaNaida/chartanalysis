a010bae2c17f42fa0559f645f150df0cc007cdc0
AdityaNaida <adityanaida@gmail.com>
2024-05-17 12:10:06 +0530

    [ADD] Menu and Timer Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const plansBtn = document.querySelector(".plans-btn");
 const querryBtn = document.querySelector(".querries-btn");
+const nestedNavbar = document.querySelector(".nested-nav");
 
 function activeQuerry() {
   querryBtn.classList.add("bg-orange");
@@ -10,3 +11,57 @@ function activePlans() {
   querryBtn.classList.remove("bg-orange");
   plansBtn.classList.add("bg-orange");
 }
+
+function fadeInMenu() {
+  nestedNavbar.classList.add("toFadein");
+  nestedNavbar.classList.remove("toFadeout");
+}
+
+function fadeOutMenu() {
+  nestedNavbar.classList.add("toFadeout");
+  nestedNavbar.classList.remove("toFadein");
+}
+
+const coundownElement = document.querySelector(".count-down");
+const coundownTime = 15 * 24 * 60 * 60 * 1000; //15 days in miliseconds
+const localStorageKey = "coundownEndTime";
+
+function getRemainingtime(endTime) {
+  const now = new Date().getTime();
+  return endTime - now;
+}
+
+function formatTime(miliseconds) {
+  const totalSeconds = Math.floor(miliseconds / 1000);
+  const days = Math.floor(totalSeconds / (24 * 60 * 60));
+  const hours = Math.floor((totalSeconds % (24 * 60 * 60)) / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  return `${days}<span>days</span>  : ${hours}<span>hrs</span> : ${minutes}<span>min</span> : ${seconds}<span>sec</span>`;
+}
+
+function startCoundown() {
+  let endTime = localStorage.getItem(localStorageKey);
+
+  if (!endTime || getRemainingtime(endTime) <= 0) {
+    endTime = new Date().getTime() + coundownTime;
+    localStorage.setItem(localStorageKey, endTime);
+  }
+
+  const updateTime = () => {
+    const remainingTime = getRemainingtime(endTime);
+
+    if (remainingTime <= 0) {
+      endTime = new Date().getTime() + coundownTime;
+      localStorage.setItem(localStorageKey, endTime);
+    } else {
+      coundownElement.innerHTML = formatTime(remainingTime);
+    }
+  };
+
+  updateTime();
+  setInterval(updateTime, 1000);
+}
+
+startCoundown();
