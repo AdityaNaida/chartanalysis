22aafba26b73a18ffeba36d954b98897751e5418
AdityaNaida <adityanaida@gmail.com>
2024-06-18 18:18:26 +0530

    [ADD] Slider using swiper.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,3 +78,21 @@ function rightClick() {
 function leftClick() {
   testimoniSlider.scrollLeft -= testimoniSlider.scrollWidth
 }
+
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
